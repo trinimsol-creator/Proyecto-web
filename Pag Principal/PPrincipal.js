58f6bc7764cdc71878ca6283e2195e955450c36d
trinimsol-creator <trinimsol@gmail.com>
2025-10-16 17:01:33 -0300

    Ultimossss

--- Pag Principal/PPrincipal.js
+++ Pag Principal/PPrincipal.js
@@ -4,13 +4,15 @@
     function showSlides() {
         let i;
         let slides = document.getElementsByClassName("mySlides");
+
         for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";  
         }
+
         slideIndex++;
         if (slideIndex > slides.length) {slideIndex = 1}    
         slides[slideIndex-1].style.display = "block";  
-        setTimeout(showSlides, 5000); 
+        setTimeout(showSlides, 4000); 
     }
 
 
--- admin1form/Lista_de_Pedidos.js
+++ admin1form/Lista_de_Pedidos.js
@@ -2,18 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
    
     var listaDePedidos = document.querySelectorAll(".pedido");
-    var campoBusqueda = document.getElementById("buscar");
 
     
     for (var i = 0; i < listaDePedidos.length; i++) {
         var pedidoActual = listaDePedidos[i];
 
-        // Extraemos el número del pedido
         var textoPedido = pedidoActual.querySelector("h3").textContent;
         var numeroPedido = textoPedido.match(/\d+/)[0];
         var claveLocalStorage = "estado_" + numeroPedido;
 
-        // Buscamos si hay un estado guardado y lo mostramos
         var estadoGuardado = localStorage.getItem(claveLocalStorage);
         if (estadoGuardado) {
             var parrafos = pedidoActual.querySelectorAll("p");
@@ -33,20 +30,4 @@ document.addEventListener("DOMContentLoaded", function() {
             this.style.transform = "scale(1)";
         });
     }
-
-    
-    campoBusqueda.addEventListener("input", function() {
-        var textoBuscado = campoBusqueda.value.toLowerCase();
-
-        for (var k = 0; k < listaDePedidos.length; k++) {
-            var pedidoARevisar = listaDePedidos[k];
-            var textoCompleto = pedidoARevisar.textContent.toLowerCase();
-
-            if (textoCompleto.indexOf(textoBuscado) !== -1) {
-                pedidoARevisar.style.display = "block";
-            } else {
-                pedidoARevisar.style.display = "none";
-            }
-        }
-    });
 });
