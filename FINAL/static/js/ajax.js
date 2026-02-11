d80714b971831aab51c733e9415d4242fabc7d72
Belufrane <mariafranetovch@uca.edu.ar>
2026-02-11 16:08:48 -0300

    sin funciones flecha

--- FINAL/static/js/ajax.js
+++ FINAL/static/js/ajax.js
@@ -1,88 +1,139 @@
-function queryAjax(url, targetId = "", method = "POST", data = null) {
-  const xhr = new XMLHttpRequest();
+function queryAjax(url, idDest, method = "POST", dataSend = null) {
 
-  xhr.open(method, url, true);
+    const xhr = new XMLHttpRequest();     
 
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
+    if (xhr) {
+        xhr.timeout = 2000;
+        xhr.open(method, url, true);
+        document.body.style.cursor = 'wait';
 
-      if (xhr.status === 200) {
-        if (targetId) {
-          document.getElementById(targetId).innerHTML = xhr.responseText;
-        }
-      } else {
-        console.error("Error AJAX:", xhr.status, xhr.responseText);
-        alert("Ocurrió un error al comunicarse con el servidor.");
-      }
+        xhr.onload = function () {
+            document.body.style.cursor = 'default';
 
+            if (xhr.status === 200) {
+                if (idDest) {
+                    setDataIntoNode(idDest, xhr.responseText);
+                }
+            } else {
+                console.error("Error AJAX:", xhr.status, xhr.responseText);
+                alert("Ocurrió un error al comunicarse con el servidor.");
+            }
+        };
+
+        xhr.ontimeout = function () {
+            console.log("Terminado por expiración de tiempo");
+        };
+
+        xhr.onloadend = function () {
+            document.body.style.cursor = 'default';
+        };
+
+        xhr.send(dataSend);
     }
-  };
-
-  xhr.send(data);
+    else {
+        console.log('No se pudo instanciar el objeto AJAX!');
+    }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+function setDataIntoNode(idDest, textHTML) {
 
-  document.querySelectorAll(".cantidad").forEach(input => {
-    input.addEventListener("change", cambiarCantidad);
-  });
+    var oElement;
+    var sNameTag;
+    var elementsReadOnlyInnerHTML;
 
-  document.querySelectorAll(".remove-btn").forEach(btn => {
-    btn.addEventListener("click", eliminarProducto);
-  });
+    elementsReadOnlyInnerHTML = [
+        "INPUT","COL","COLGROUP","FRAMESET","HEAD","HTML",
+        "STYLE","TABLE","TBODY","TFOOT","THEAD","TITLE","TR"
+    ];
+
+    if (document.getElementById(idDest)) {
+        oElement = document.getElementById(idDest);
+        sNameTag = oElement.tagName.toUpperCase();
+
+        if (elementsReadOnlyInnerHTML.indexOf(sNameTag) === -1) {
+            oElement.innerHTML = textHTML;
+        }
+        else if (sNameTag === 'INPUT') {
+            oElement.value = textHTML;
+        }
+        else {
+            setAnyInnerHTML(oElement, textHTML);
+        }
+    }
+    else {
+        console.log('El elemento destino cuyo id="' + idDest + '" no existe!');
+    }
+}
+
+function setAnyInnerHTML(oElement, html) {
+    var temp = oElement.ownerDocument.createElement('div');
+    temp.innerHTML = html;
+    oElement.parentNode.replaceChild(temp.firstChild.firstChild, oElement);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    var inputs = document.querySelectorAll(".cantidad");
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener("change", cambiarCantidad);
+    }
+
+    var botones = document.querySelectorAll(".remove-btn");
+    for (var j = 0; j < botones.length; j++) {
+        botones[j].addEventListener("click", eliminarProducto);
+    }
 
 });
 
-
 function cambiarCantidad(e) {
-  const card = e.target.closest(".product-card");
-  const idProducto = card.dataset.id;
-  const cantidad = e.target.value;
+    var card = e.target.closest(".product-card");
+    var idProducto = card.dataset.id;
+    var cantidad = e.target.value;
 
-  let data = new FormData();
-  data.append("id", idProducto);
-  data.append("cantidad", cantidad);
+    var data = new FormData();
+    data.append("id", idProducto);
+    data.append("cantidad", cantidad);
 
-  queryAjax("/ajax/actualizar_carrito", "total-carrito", "POST", data);
+    queryAjax("/ajax/actualizar_carrito", "total-carrito", "POST", data);
 }
-
 function eliminarProducto(e) {
-  const card = e.target.closest(".product-card");
-  const idProducto = card.dataset.id;
+    var card = e.target.closest(".product-card");
+    var idProducto = card.dataset.id;
 
-  let data = new FormData();
-  data.append("id", idProducto);
+    var data = new FormData();
+    data.append("id", idProducto);
 
-  queryAjax("/ajax/eliminar_producto", "total-carrito", "POST", data);
+    queryAjax("/ajax/eliminar_producto", "total-carrito", "POST", data);
 
-  card.remove();
+    card.remove();
 }
-
 function agregarCarrito(idProducto) {
-  const cantidad = document.getElementById("cantidad").value;
+    var cantidad = document.getElementById("cantidad").value;
 
-  let data = new FormData();
-  data.append("id", idProducto);
-  data.append("cantidad", cantidad);
+    var data = new FormData();
+    data.append("id", idProducto);
+    data.append("cantidad", cantidad);
 
-  queryAjax("/ajax/agregar_carrito", "", "POST", data);
+    queryAjax("/ajax/agregar_carrito", "", "POST", data);
 
-  mostrarToast("Producto agregado al carrito");
+    mostrarToast("Producto agregado al carrito");
 }
 
 function mostrarToast(texto) {
-  const toast = document.createElement("div");
-  toast.className = "toast-msg";
-  toast.innerText = texto;
+    var toast = document.createElement("div");
+    toast.className = "toast-msg";
+    toast.innerText = texto;
 
-  document.body.appendChild(toast);
+    document.body.appendChild(toast);
 
-  setTimeout(() => {
-    toast.classList.add("show");
-  }, 10);
+    setTimeout(function () {
+        toast.classList.add("show");
+    }, 10);
 
-  setTimeout(() => {
-    toast.classList.remove("show");
-    setTimeout(() => toast.remove(), 300);
-  }, 2500);
+    setTimeout(function () {
+        toast.classList.remove("show");
+        setTimeout(function () {
+            toast.remove();
+        }, 300);
+    }, 2500);
 }
